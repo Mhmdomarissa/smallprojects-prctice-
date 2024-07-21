8108d9439bab468819a99f9999c8acfe42480786
Mhmdomarissa <mhmdomarissa1@gmail.com>
2024-07-21 17:26:25 +0300

    practice in ifelse witch statements revesion

---
+++ scripts/index.js
@@ -0,0 +1,62 @@
+//ternrary operator
+// let age = 21 
+// let messege = age >=18 ? console.log('You are old enough to drive') : console.log('You are not')
+
+
+// let time = 16 ; 
+// let greeting = time >= 6 && time < 12 ? 'Good Morning' : time >= 12 && time < 18 ? 'Good Afternoon' : time >= 18 && time < 22 ? 'Good Evening' : 'Good Night' ;
+// console.log(greeting);
+
+// let purcahseamount = 125 ; 
+// let discount = purcahseamount >= 100 ? 10 : 0 ; 
+// console.log(`your total is $ ${purcahseamount - purcahseamount * (discount)/100}`)
+
+//switch 
+// let day = 1 ; 
+// switch(day){
+//     case 1 :
+//         console.log(`it is monday`);
+//         break;
+//     case 2 :
+//         console.log(`it is tuesday`);
+//         break;
+//     case 3 : 
+//      console.log(`it is wednesday`);
+//      break;
+//     case 4 :
+//         console.log(`it is thursday`);
+//         break;
+//     case 5 :
+//         console.log(`it is friday`);
+//         break;
+//     case 6 :
+//         console.log(`it is saturday`);
+//         break;
+//     case 7 :
+//         console.log(`it is sunday`);
+//         break;
+//     default :
+//     console.log(`Invalid day number`);
+
+// }
+
+
+let testScore = 92 ; 
+let grade ;
+ 
+switch(true){
+    case testScore >= 90 :
+        grade = 'A';
+        break;
+    case testScore >= 80 :
+        grade = 'B';
+        break;
+    case testScore >= 70 :
+        grade = 'C';
+        break;
+    case testScore >= 60 :
+        grade = 'D';
+        break;
+    default :
+        grade = 'F';
+}
